c11293912a36970e9650bc35c0e22fa25e41c7ad
EvaLambrecht <123171835+EvaLambrecht@users.noreply.github.com>
2023-01-25 14:54:25 +0100

    Module 1 and 2 Folder

---
+++ Module 1 and 2/src/employees-json.js
@@ -0,0 +1,31 @@
+const DATA = {
+
+    employees: [
+      {
+       id: 1,
+       name: "Peter",
+       surname: "Peterson",
+       department: "IT"
+      },
+      {
+        id: 2,
+        name: "Simon",
+        surname: "Sayer",
+        department: "Sales"
+       },
+       {
+        id: 3,
+        name: "Hugo",
+        surname: "Port",
+        department: "Finance"
+       },
+       {
+        id: 4,
+        name: "Hugo",
+        surname: "Jusic",
+        department: "ABC"
+       }
+    ]
+
+   }
+   
